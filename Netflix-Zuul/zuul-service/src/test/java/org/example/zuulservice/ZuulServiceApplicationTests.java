1d7c04c77abfcff38ede2b9a8fd8e7f6efba5bc6
김태헌 <tae77777@naver.com>
2024-02-02 14:40:08 +0900

    FEAT : Zuul-Service 생성

---
+++ Netflix-Zuul/zuul-service/src/test/java/org/example/zuulservice/ZuulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.zuulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
