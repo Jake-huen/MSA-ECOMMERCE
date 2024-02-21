40f9622ac02f3b4651ae867de398894133043152
김태헌 <tae77777@naver.com>
2024-02-21 10:57:07 +0900

    FEAT : Micrometer 구현

--- microservice/user-service/src/main/java/org/example/userservice/controller/UserController.java
+++ microservice/user-service/src/main/java/org/example/userservice/controller/UserController.java
@@ -1,5 +1,6 @@
 package org.example.userservice.controller;
 
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.example.userservice.dto.UserDto;
@@ -28,6 +29,7 @@ public class UserController {
     private final Greeting greeting;
 
     @GetMapping("/health_check")
+    @Timed(value = "users.status", longTask = true)
     public String status() {
         return String.format("It's Working in User Service"
                 + ", port(local.server.port)=" + env.getProperty("local.server.port")
@@ -37,6 +39,7 @@ public class UserController {
     }
 
     @GetMapping("/welcome")
+    @Timed(value = "users.welcome", longTask = true)
     public String welcome() {
         // return env.getProperty("greeting.message");
         return greeting.getMessage();
